3664ac029ec6f43c7ee9860dae07b1b6b898a330
Gustavo Aurelio <gustavogontijoo@gmail.com>
2021-06-09 00:58:11 -0300

    correcao no mapa

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,7 @@
 import { Map, TileLayer, Marker } from 'react-leaflet'
 import { LeafletMouseEvent } from 'leaflet'
 import MarkerIcon from '../../utils/markerIcon'
+import { useEffect, useState } from 'react'
 
 interface PropsMapContainer {
   handleMapClick: (event: LeafletMouseEvent) => void
@@ -10,16 +11,24 @@ interface PropsMapContainer {
   }
 }
 const MapContainer: React.FC<PropsMapContainer> = (props) => {
+  const [latitude, setLatitude] = useState(-19.9207105)
+  const [longitude, setLongitude] = useState(-43.9952684)
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLatitude(position.coords.latitude)
+      setLongitude(position.coords.longitude)
+    })
+  }, [])
   return (
     <Map
-      center={[-19.8736366, -44.9973021]}
+      center={[latitude, longitude]}
       zoom={15}
       style={{ width: '100%', height: '100%' }}
       onclick={props.handleMapClick}
     >
       {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
       <TileLayer
-        url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAP}`}
+        url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${ process.env.NEXT_PUBLIC_MAP }`}
       />
 
       {props.position.latitude !== 0 && (
